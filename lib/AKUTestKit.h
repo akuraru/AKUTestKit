e6fcd853a4fc6964e1fdf9657506db8b24b3058c
akuraru <akuraru@gmail.com>
2014-04-17 12:30:02 +0900

    add AKUTestKit.h

---
+++ lib/AKUTestKit.h
@@ -0,0 +1,17 @@
+//
+//  Newline_AKUTestKit.h
+//  AKUTestKit
+//
+//  Created by akuraru on 2014/04/17.
+//
+//
+
+#import "AKURandString.h"
+
+#import "AKUDateGenerator.h"
+#import "AKUNumberGenerator.h"
+#import "AKUFixedDate.h"
+#import "AKULoseDictionaryGenarator.h"
+#import "AKUListFunctional.h"
+#import "AKURegularString.h"
+#import "AKUStringGenerator.h"
